5cd6cf150cb11be08433d8153b6e53c3c71dbb1b
Tony Jin <tonyjin@secondspectrum.com>
2020-02-29 16:13:47 +0800

    Setup blog server

--- src/blog-server/index.js
+++ src/blog-server/index.js
@@ -4,18 +4,23 @@ import send from 'koa-send';
 
 const router = new Router();
 
-const STATIC_PATH = '';
+const STATIC_PATH = `${__dirname}/../../public`;
 
 router.get('/(.*)', async ctx => {
-  const path = ctx.path;
-  await send(ctx, path, { root:  STATIC_PATH})
-})
+  const path = ctx.path === '/' ? '/index.html' : ctx.path;
+
+  try {
+    await send(ctx, path, { root: STATIC_PATH });
+  } catch (error) {
+    console.error(error);
+
+    ctx.status = error.status;
+    ctx.body = ctx.status === 404 ? 'Not found.' : 'Error';
+  }
+});
 
 const app = new Koa();
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Blog server is listening at 3000...'));
-
